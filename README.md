e1ddddb4e131108271f3f88aeb5dcdc3fe88a397
abaccam <abaccam.simplon@gmail.com>
2018-03-02 11:46:22 +0100

    createPoll

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# promo5-vue-monPoll
-poll app avec vue.js
+# promo5-vue-poll
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/CreatePoll.vue
@@ -0,0 +1,47 @@
+<template>
+  <button type="submit" value="Submit">Submit</button>
+</template>
+
+<script>
+import List from './Choise.vue';
+export default {
+  
+
+  name:'CreatePoll',
+    components: {
+        Choise
+    },
+    /**
+     * La propriété data du component est une fonction qui return un objet
+     * contenant la liste des variables que l'on souhaite rendre accessible
+     * dans le template.
+     */
+    data: function(){
+        return {
+            tab: ['ga', 'zo', 'bu', 'meu', 'ga']
+        }
+    },
+    /**
+     * La propriété methods du component est un objet contenant les 
+     * méthodes que l'on veut rendre accessible au template
+     */
+    methods: {
+        show() {
+            alert('kekchoz');
+        },
+        showItem(item) {
+            console.log(item);
+        },
+        remove(nb){
+            //Pour accèder à une variable data ou une méthode
+            // de notre component, on peut utiliser le this
+            this.tab.splice(nb, 1)
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
